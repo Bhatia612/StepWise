f331078cc15c2a8490e0cfd259f24cdcda6393d1
MOHIT BHATIA <mohitbhatia612@gmail.com>
2026-06-15 18:14:50 -0400

    feat: setup mongoose db connection with server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-
+const connectDB = require("./config/db");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// --- Global Middlewares ---
+connectDB();
+
 app.use(cors());
 app.use(express.json());
 
-// --- Health Check Route ---
 app.get("/", (req, res) => {
   res.json({ message: "StepWise is running" });
 });

c06f806431bba51d419bd0bd7184264cadf43779
MOHIT BHATIA <mohitbhatia612@gmail.com>
2026-06-21 13:33:12 -0400

    feat: added auth controller with signup, login, and logout using JWT and httpOnly cookies

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,106 @@
+const jwt = require("jsonwebtoken")
+const User = require("../models/user.model")
+
+const genrateToken = (userId) => {
+    return jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: "2d",
+    })
+}
+
+const setAuthCookies = (res, token) => {
+    res.cookies("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "lax",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+    })
+}
+
+const signUpController = (req, res, next) => {
+    try {
+        const { username, email, password } = req.body
+
+        const existingUser = await User.findOne({ $or: [{ username }, { email }] })
+
+
+        if (existingUser) {
+            return res.status(400).json({
+                success: false,
+                message: "Username or email already in use",
+            });
+        }
+
+        const user = await User.create({
+            username,
+            email,
+            password
+        })
+
+        const token = genrateToken(user._id)
+        setAuthCookies(res, token)
+
+        res.status(200).json({
+            success: true,
+            data: {
+                id: user._id,
+                username: user.username,
+                email: user.email,
+            }
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+const logInController = (req, res, next) => {
+    try {
+        const { identifier, password } = req.body
+
+        const user = await User.findOne({
+            $or: [{ username: identifier }, { email: identifier }],
+        });
+
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid credentials",
+            })
+        }
+
+        const isMatched = await user.comparePassword(password)
+
+        if (!isMatch) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid credentials",
+            });
+        }
+
+        const token = generateToken(user._id);
+        setAuthCookie(res, token);
+
+
+        res.status(200).json({
+            success: true,
+            data: {
+                id: user._id,
+                username: user.username,
+                email: user.email,
+            },
+        });
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+const logOutController = (req, res) => {
+    res.clearCookie("token");
+    res.status(200).json({
+        success: true,
+        message: "Logged out successfully",
+    });
+};
+
+module.exports = { signUpController, logInController, logOutController };
